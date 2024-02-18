b8be8a2c4c3f7cc3bd564cdf06fcefee7d65560d
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-02-18 22:41:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// console.log(window)
+// console.dir(document)
+
+// console.log(window.document)
+// console.dir(window.document)
+
+// let id=document.getElementById("heading")
+// console.log(id)
+
+// let clas=document.getElementsByClassName("Babar")
+// console.log(clas)
+
+// let elements=document.querySelector("p")
+// console.dir(elements)
+// let allElements=document.querySelectorAll("p")
+// console.dir(allElements)
+
+document.querySelector("div")
